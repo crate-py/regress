54cf905e3e71edeb50e912e1649d895ba64f7ed8
Julian Berman <Julian@GrayVines.com>
2023-03-14 11:44:47 -0400

    First few wrapped functions.

---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+use pyo3::{create_exception, prelude::*, types::PySlice};
+use regress::{Match, Range, Regex};
+
+create_exception!(regress, RegressError, pyo3::exceptions::PyException);
+
+#[repr(transparent)]
+#[pyclass(name = "Regex", module = "regress", frozen)]
+struct RegexPy {
+    inner: Regex,
+}
+
+#[pymethods]
+impl RegexPy {
+    #[new]
+    fn init(value: &str) -> PyResult<Self> {
+        match Regex::new(value) {
+            Ok(inner) => Ok(RegexPy { inner }),
+            Err(e) => Err(RegressError::new_err(e.to_string())),
+        }
+    }
+
+    fn find(&self, value: &str) -> Option<MatchPy> {
+        match self.inner.find(value) {
+            Some(inner) => Some(MatchPy { inner }),
+            None => None,
+        }
+    }
+
+    fn find_iter(&self, value: &str) -> Vec<MatchPy> {
+        self.inner
+            .find_iter(value)
+            .map(|m| MatchPy { inner: m })
+            .collect()
+    }
+}
+
+#[repr(transparent)]
+#[pyclass(name = "Match", module = "regress", frozen)]
+struct MatchPy {
+    inner: Match,
+}
+
+#[pymethods]
+impl MatchPy {
+    fn group(&self, idx: usize, py: Python) -> PyResult<Option<PyObject>> {
+        match self.inner.group(idx) {
+            Some(m) => Ok(Some(to_slice(py, m)?)),
+            None => Ok(None),
+        }
+    }
+
+    fn named_group(&self, name: &str, py: Python) -> PyResult<Option<PyObject>> {
+        match self.inner.named_group(name) {
+            Some(m) => Ok(Some(to_slice(py, m)?)),
+            None => Ok(None),
+        }
+    }
+
+    fn range(&self, py: Python) -> PyResult<PyObject> {
+        to_slice(py, self.inner.range())
+    }
+}
+
+#[pymodule]
+#[pyo3(name = "regress")]
+fn regress_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<MatchPy>()?;
+    m.add_class::<RegexPy>()?;
+    Ok(())
+}
+
+fn to_slice(py: Python, range: Range) -> PyResult<PyObject> {
+    Ok(PySlice::new(py, range.start.try_into()?, range.end.try_into()?, 1).into())
+}
---
+++ tests/test_regress.py
@@ -0,0 +1,51 @@
+import regress
+
+
+def test_string_contains_match():
+    """
+    https://docs.rs/regress/latest/regress/#example-test-if-a-string-contains-a-match
+    """
+    regex = regress.Regex(r"\d{4}")
+    assert regex.find("2020-20-05") is not None
+
+
+def test_iterating_over_matches():
+    """
+    https://docs.rs/regress/latest/regress/#example-iterating-over-matches
+    """
+
+    regex = regress.Regex(r"(\w)\1")
+    text = "Frankly, Miss Piggy, I don't give a hoot!"
+    result = [text[m.range()] for m in regex.find_iter(text)]
+    assert result == ["ss", "gg", "oo"]
+
+
+def test_capture_groups():
+    """
+    https://docs.rs/regress/latest/regress/#example-using-capture-groups
+    """
+
+    regex = regress.Regex(r"(\d{4})")
+    text = "Today is 2020-20-05"
+
+    match = regex.find(text)
+    assert match is not None
+
+    group = match.group(1)
+    assert group
+
+    assert text[group] == "2020"
+
+
+def test_named_group():
+    regex = regress.Regex(r"(?<animal>fox|dog)")
+
+    text = "The quick brown fox jumped over the lazy dog's back"
+
+    match = regex.find(text)
+    assert match is not None
+
+    group = match.named_group("animal")
+    assert group
+
+    assert text[group] == "fox"
